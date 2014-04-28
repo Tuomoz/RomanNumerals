c950463b9e9706da4589afd4ea70df7ddab0c8bb
Tuomo Kärkkäinen <tuomon.email@gmail.com>
2014-04-28 19:32:27 +0300

    Initial commit

---
+++ src/romannumerals/RomanNumerals.java
@@ -0,0 +1,15 @@
+package romannumerals;
+
+/**
+ *
+ * @author Tuomo Kärkkäinen
+ */
+public class RomanNumerals
+{
+
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
