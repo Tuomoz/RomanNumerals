4eb1aa6a88d5051600f81ce05ec4917faea62186
Tuomo Kärkkäinen <tuomon.email@gmail.com>
2014-04-29 20:20:28 +0300

    Command line usage

--- src/romannumerals/RomanNumerals.java
+++ src/romannumerals/RomanNumerals.java
@@ -1,7 +1,9 @@
 package romannumerals;
 
 /**
- *
+ * An application for converting Roman numbers into integers. The program takes
+ * a list of numbers to be converted as command line parameters.
+ * 
  * @author Tuomo Kärkkäinen
  */
 public class RomanNumerals
@@ -9,7 +11,19 @@ public class RomanNumerals
 
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        RomanNumeralsConverter converter = new RomanNumeralsConverter();
+        if (args.length > 0) {
+            for (String romanNumber : args) {
+                int converted = converter.convert(romanNumber);
+                if (converted != -1) {
+                    System.out.println(romanNumber + " = " + converted);
+                } else {
+                    System.out.println(romanNumber + " is not a valid Roman number.");
+                }
+            }
+        } else {
+            System.out.println("Please give the numbers to be converted as command line parameters.");
+        }
     }
     
 }
